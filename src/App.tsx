fb819ca6c02600971d681b250f415b4572da91db
mechanizzed <dsibr@hotmail.com>
2020-08-03 12:02:24 -0300

    Add: styles and images

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import './assets/styles/global.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial</h1>
+      <h1>Initial new</h1>
     </div>
   );
 }
