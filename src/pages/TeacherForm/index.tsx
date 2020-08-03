cfe7ea56c78fca6a407894754fb64f01a4e79f83
mechanizzed <dsibr@hotmail.com>
2020-08-03 14:53:17 -0300

    Add: react-router-dom and routes initial setup

---
+++ src/pages/TeacherForm/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function TeacherForm() {
+  return <h1>Form professor</h1>;
+}
+
+export default TeacherForm;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+function TeacherList() {
+  return <h1>Professores</h1>;
+}
+
+export default TeacherList;
