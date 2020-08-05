301eddf310add202aa93cb78e2741286e2d93954
mechanizzed <dsibr@hotmail.com>
2020-08-05 14:03:35 -0300

    Udpate: styles and forms

--- src/pages/TeacherForm/index.tsx
+++ src/pages/TeacherForm/index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 // components
 import PageHeader from '../../Components/PageHeader';
 import Input from '../../Components/Input';
+import Textarea from '../../Components/Textarea';
+import Select from '../../Components/Select';
 
 // images and icons
 import WarningIcon from '../../assets/images/icons/warning.svg';
@@ -23,13 +25,54 @@ function TeacherForm() {
           <legend>Seus dados</legend>
           <Input name="name" label="Nome completo" />
           <Input name="avatar" label="Avatar" />
-          <Input name="whatsapp" label="whatsapp" />
+          <Input name="whatsapp" label="WhatsApp" />
+          <Textarea name="bio" label="Biografia" />
         </fieldset>
 
         <fieldset>
           <legend>Sobre a aula</legend>
-          <Input name="subject" label="Matéria" />
+          <Select
+            name="subject"
+            label="Matéria"
+            options={[
+              { value: 'Artes', label: 'Artes' },
+              { value: 'Biologia', label: 'Biologia' },
+              { value: 'Ciências', label: 'Ciências' },
+              { value: 'Educação física', label: 'Educação física' },
+              { value: 'Física', label: 'Física' },
+              { value: 'Geografia', label: 'Geografia' },
+              { value: 'História', label: 'História' },
+              { value: 'Matemática', label: 'Matemática' },
+              { value: 'Portugês', label: 'Portugês' },
+              { value: 'Química', label: 'Química' },
+            ]}
+          />
           <Input name="cost" label="Custo da sua hora por aula" />
+        </fieldset>
+
+        <fieldset>
+          <legend>
+            Horários disponíveis
+            <button type="button">+ Novo horário</button>
+          </legend>
+
+          <div className="schedule-item">
+            <Select
+              name="week_day"
+              label="Dia da semana"
+              options={[
+                { value: '0', label: 'Domingo' },
+                { value: '1', label: 'Segunda-feira' },
+                { value: '2', label: 'Terça-feira' },
+                { value: '3', label: 'Quarta-feira' },
+                { value: '4', label: 'Quinta-feira' },
+                { value: '5', label: 'Sexta-feira' },
+                { value: '6', label: 'Sábado' },
+              ]}
+            />
+            <Input name="from" label="Das" type="time" />
+            <Input name="to" label="Até" type="time" />
+          </div>
         </fieldset>
 
         <footer>
